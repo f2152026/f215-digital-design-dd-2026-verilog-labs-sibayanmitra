62604e5c5eee2daeaf404a59a775feb7c0d02f54
sibayanmitra <nirbanmitra.2018@gmail.com>
2026-09-19 16:15:31 +0000

    lab02 task1

--- labs/lab02/task1/mux_df.v
+++ labs/lab02/task1/mux_df.v
@@ -9,9 +9,9 @@ module mux_df (
   input      I0,
   input      I1,
   input      S,
-  output reg Y
+  output wire Y
 );
 
-  assign Y = S ? I1 : I0;
+  assign Y = (S & I0) | ((~S) & I1);
 
 endmodule
